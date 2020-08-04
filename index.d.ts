1639eedfd04a9f0145ea716be26bc59fdbe97df0
Giovanni Cardamone <g.cardamone2@gmail.com>
2020-08-04 17:11:53 +0200

    fixed optional values in definitions

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 declare module "@ptkdev/logger" {
 	interface RotateType {
 		size: "10B" | "10K" | "10M" | "10G";
-		encoding: string;
+		encoding?: string;
 	}
 
 	interface PaletteElement {
@@ -11,14 +11,14 @@ declare module "@ptkdev/logger" {
 	}
 
 	interface PaletteType {
-		info: PaletteElement;
-		warning: PaletteElement;
-		error: PaletteElement;
-		stackoverflow: PaletteElement;
-		docs: PaletteElement;
-		debug: PaletteElement;
-		sponsor: PaletteElement;
-		time: Omit<PaletteElement, "text">;
+		info?: PaletteElement;
+		warning?: PaletteElement;
+		error?: PaletteElement;
+		stackoverflow?: PaletteElement;
+		docs?: PaletteElement;
+		debug?: PaletteElement;
+		sponsor?: PaletteElement;
+		time?: Omit<PaletteElement, "text">;
 	}
 
 	interface PathType {
@@ -27,33 +27,33 @@ declare module "@ptkdev/logger" {
 	}
 
 	interface LoggerOptions {
-		language: "de" | "en" | "es" | "fr" | "it" | "pl" | "pt" | "ru";
-		colors: boolean;
-		debug: boolean;
-		info: boolean;
-		warning: boolean;
-		error: boolean;
-		sponsor: boolean;
-		write: boolean;
-		type: "log" | "json";
-		rotate: RotateType;
-		palette: PaletteType;
-		path: PathType;
+		language?: "de" | "en" | "es" | "fr" | "it" | "pl" | "pt" | "ru";
+		colors?: boolean;
+		debug?: boolean;
+		info?: boolean;
+		warning?: boolean;
+		error?: boolean;
+		sponsor?: boolean;
+		write?: boolean;
+		type?: "log" | "json";
+		rotate?: RotateType;
+		palette?: PaletteType;
+		path?: PathType;
 	}
 
 	export default class Logger {
 		constructor(options?: LoggerOptions);
 
-		debug(message: string, tag?: string): void;
-		info(message: string, tag?: string): void;
-		warning(message: string, tag?: string): void;
-		error(message: string, tag?: string): void;
-		sponsor(message: string, tag?: string): void;
-		stackoverflow(
+		debug?(message: string, tag?: string): void;
+		info?(message: string, tag?: string): void;
+		warning?(message: string, tag?: string): void;
+		error?(message: string, tag?: string): void;
+		sponsor?(message: string, tag?: string): void;
+		stackoverflow?(
 			message: string,
 			tag?: string,
 			error_string?: string
 		): void;
-		docs(message: string, url?: string, tag?: string): void;
+		docs?(message: string, url?: string, tag?: string): void;
 	}
 }
