736dbc05c7026a3b5629474ae772e38adc1b9cff
Patryk Rzucidlo (PTKDev) <ptkdev@gmail.com>
2020-08-19 21:38:45 +0200

    [Next] v1.7.3-nightly.20200819

--- examples/example-json.js
+++ examples/example-json.js
@@ -29,7 +29,7 @@ const logger = new Logger(options);
 logger.info("your info message", "your tag");
 logger.warning("your warning message", "your tag");
 logger.error("your error message", "your tag");
-logger.debug(JSON.stringify(options), "your tag");
+logger.debug(`your debug message`, "your tag");
 logger.sponsor("your sponsor message", "your tag");
 logger.stackoverflow("your stackoverflow message", "your tag");
 logger.docs("your docs message", "https://docs.yoursite.com", "your tag");
