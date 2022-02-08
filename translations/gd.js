b035d6ed650e906965ea4b512dc30f39007e7b1e
Sofia Vicedomini <11537307+blacksoulgem95@users.noreply.github.com>
2022-02-08 12:08:40 +0100

    Scots Gaelic translation

---
+++ translations/gd.js
@@ -0,0 +1,26 @@
+/**
+ * Translation: Scots Gaelic
+ * =====================
+ * How translate:
+ * Chalk: chalk is npm plugin to colorize text in terminal, example: {white.bold.bgRgb(46, 204, 113) TEXT_TEXT_TEXT }
+ *        don't translate white.bold.bgRgb but translate TEXT_TEXT_TEXT before of }
+ *
+ * Keys:  don't translate keywords on left, example: {"done":"done"}
+ *        word <done> on left side is keyword, ever in English. Translate only phrases on right side of two dots
+ *
+ * Param: don't translate words between ## ##, example: ##ciao##
+ *
+ * @contributors: Sofia Vicedomini [@blacksoulgem95] <the@italianprogrammer.pizza> (https://italianprogrammer.pizza)
+ *
+ * @license: CC BY 4.0 License
+ *
+ */
+module.exports = {
+	INFO: " | FIOSRACHADH   ",
+	WARNING: " | RABHADH     ",
+	ERROR: " | MEARACHD         ",
+	DEBUG: " | DEBUG          ",
+	DOCS: " | SGRÌOBHAINNEAN ",
+	STACKOVERFLOW: " | STACKOVERFLOW  ",
+	SPONSOR: " | URRASAIR        "
+};
